49b8dbb9c0a1696d3379c176ccab7a79981bb17e
ricmaps <ricardomapurungajunior@gmail.com>
2026-08-17 00:07:22 -0300

    update neuwld

--- packages/neuwld/default.nix
+++ packages/neuwld/default.nix
@@ -20,8 +20,8 @@ stdenv.mkDerivation {
   src = fetchFromSourcehut {
     owner = "~shrub900";
     repo = "neuwld";
-    rev = "38c3b49aea1a1e4fc3fe4d6938f18307bad4c979";
-    hash = "sha256-9/m4jFyQf5pOzG15q3a2Rk3MV1rUceLzt9kHiv4Db9U=";
+    rev = "554f827cadfdfcc276c709dbffa3b2b04c70cf7c";
+    hash = "sha256-KAK4/TpNekaonN0yxi4/5mRdZL1uxYdGmwl41FRH5wU=";
   };
 
   nativeBuildInputs = [
